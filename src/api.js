1fb239f720b854b059f8a371452805b74f132b26
atharvakhode <atharvakhode3@gmail.com>
2023-08-16 23:00:41 +0530

    code updated

---
+++ src/api.js
@@ -0,0 +1,17 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		'X-RapidAPI-Key': 'ea5f00e6acmsh5a062ac23753cebp1b2f0bjsn96dad22855b8',//  rapid api key here
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+    };
+    export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+    
+    export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+    export const WEATHER_API_KEY = '9a7c2262cdf02af040f85178976b6af6'; //  key from openweather API
+
+
+
+
+
+
